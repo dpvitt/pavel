58b7c3cf3f8060ee12b1786640ff84f47f4c6378
dpvitt <daniel@danielpavitt.com>
2015-11-05 16:52:06 +0000

    add gulp open task

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 
 var watch = require('./tasks/watch.js'),
 	connect = require('./tasks/connect.js'),
-	serve = require('./tasks/serve.js');
+	serve = require('./tasks/serve.js'),
+	open = require('./tasks/open.js');
 
 watch();
 connect();
+open();
 serve();
--- tasks/serve.js
+++ tasks/serve.js
@@ -6,7 +6,7 @@ module.exports = function () {
 
 	gulp.task('serve', function() {
 		runSequence(
-			['connect', 'watch']
+			['connect', 'watch', 'open']
 		);
 	});
 
