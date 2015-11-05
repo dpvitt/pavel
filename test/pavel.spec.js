d1a5c7321aa3ff173af47e034a85aca1a3e67d42
dpvitt <daniel@danielpavitt.com>
2015-11-05 17:15:30 +0000

    update test desc

--- test/pavel.spec.js
+++ test/pavel.spec.js
@@ -6,12 +6,12 @@ describe("Pavel", function() {
 
 	var pavelConstructor;
 
-    it("should see pavel", function() {
+    it("should see pavel testMethod", function() {
 
     	expect(pavelConstructor.testMethod).to.be.a('function');
     });
 
-    it("should see pavel return true", function() {
+    it("should see pavel testMethod return true", function() {
 
     	expect(pavelConstructor.testMethod()).to.equal(true);
     });
